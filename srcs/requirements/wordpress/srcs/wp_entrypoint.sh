6d3141dad4f08752030ad42fc47b7d226fc08fe6
robriard <robriard@student.42.fr>
2021-06-10 20:57:10 +0200

    end of the day

--- srcs/requirements/wordpress/srcs/wp_entrypoint.sh
+++ srcs/requirements/wordpress/srcs/wp_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #test
-cp /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+cp  /var/www/html/wp-config.php /var/www/html/wp-config-sample.php
 sed -i 's/votre_nom_de_bdd/'$WORDPRESS_DB_NAME'/' /var/www/html/wp-config.php
 sed -i 's/votre_utilisateur_de_bdd/'$WORDPRESS_DB_USER'/' /var/www/html/wp-config.php
 sed -i 's/votre_mdp_de_bdd/'$WORDPRESS_DB_PASSWORD'/' /var/www/html/wp-config.php
