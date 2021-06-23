ed5233464e0985b51b7e1ff089e9ae8b06b43443
robriard <rboriard@student.42.fr>
2021-06-23 20:36:46 +0200

    save psuh

--- makefile
+++ makefile
@@ -6,7 +6,7 @@
 #    By: robriard <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/06/08 11:41:07 by robriard          #+#    #+#              #
-#    Updated: 2021/06/08 11:41:38 by robriard         ###   ########.fr        #
+#    Updated: 2021/06/23 19:27:07 by user42           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,4 +25,4 @@ down:
 ps:
 	docker-compose -f srcs/docker-compose.yaml ps
 
-.PHONY:	up down
+.PHONY:	up down re ps
--- srcs/requirements/wordpress/srcs/wp_entrypoint.sh
+++ srcs/requirements/wordpress/srcs/wp_entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-#test
+
+
 cp  /var/www/html/wp-config.php /var/www/html/wp-config-sample.php
-sed -i 's/votre_nom_de_bdd/'$WORDPRESS_DB_NAME'/' /var/www/html/wp-config.php
-sed -i 's/votre_utilisateur_de_bdd/'$WORDPRESS_DB_USER'/' /var/www/html/wp-config.php
-sed -i 's/votre_mdp_de_bdd/'$WORDPRESS_DB_PASSWORD'/' /var/www/html/wp-config.php
+sed -i 's/database_name_here/'$WORDPRESS_DB_NAME'/' /var/www/html/wp-config.php
+sed -i 's/username_here/'$MYSQL_USER'/' /var/www/html/wp-config.php
+sed -i 's/password_here/'$MYSQL_PASSWORD'/' /var/www/html/wp-config.php
 sed -i 's/localhost/'$WORDPRESS_DB_HOST'/' /var/www/html/wp-config.php
 sed -i 's/wp_/'$WORDPRESS_TABLE_PREFIX'/' /var/www/html/wp-config.php
 chown -R www-data:www-data /var/www/html
-#
 
-echo "WordPress is up!!! "
 php-fpm7.3 -F
