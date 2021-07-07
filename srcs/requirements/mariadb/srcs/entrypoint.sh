5f2e6c92246a92db4fda56e7f608dd16dba75156
robriard <robriard@student.42.fr>
2021-07-07 03:26:28 +0200

    push of the nigth

---
+++ srcs/requirements/mariadb/srcs/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ ! -d "/var/lib/mysql/wordpress" ];
+then
+	mysql_install_db --data-dir=/var/lib/mysql
+	service mysql start
+	echo "CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci" | mysql -u root
+	echo "CREATE USER IF NOT EXISTS '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'" | mysql -u root
+	echo "GRANT ALL ON wordpress .* TO '$MYSQL_ROOT_USER'@'%'" | mysql -u root
+	echo "FLUSH PRIVILEGES;" | mysql -u root
+	service mysql stop
+fi
+#exec /usr/sbin/mysqld
+exec /bin/bash
--- srcs/requirements/wordpress/srcs/wp_entrypoint.sh
+++ srcs/requirements/wordpress/srcs/wp_entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-if  [ ! -f /var/www/wordpress/wp-config.php ];
+if  [ ! -f /var/www/html/wp-config.php ];
 then
     wp core --allow-root download --locale=fr_FR --force 
+    while  [ ! -f /var/www/html/wp-config.php ];
+    do   
+        wp core config --allow-root --dbname=wordpress --dbuser=$MYSQL_ROOT_USER --dbpass=$MYSQL_ROOT_PASSWORD --dbhost=mariadb:3306
+    done 
     wp core install --allow-root --url=$DOMAIN_NAME --title='Inception Project' --admin_user=$MYSQL_ROOT_USER --admin_password=$MYSQL_ROOT_PASSWORD  --admin_email=$MYSQL_ROOT_EMAIL --path='/var/www/html';
     wp  user create --allow-root $MYSQL_USER $MYSQL_EMAIL --user_pass=$MYSQL_PASSWORD --role=author
 fi
