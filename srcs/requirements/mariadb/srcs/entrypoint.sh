199806bc30fdf10c1b246e97ad32500e9012c43e
robriard <robriard@student.42.fr>
2021-07-07 05:46:25 +0200

    push of the nigth

--- srcs/requirements/mariadb/srcs/entrypoint.sh
+++ srcs/requirements/mariadb/srcs/entrypoint.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "/var/lib/mysql/wordpress" ];
 then
-	mysql_install_db --data-dir=/var/lib/mysql
+	mysql_install_db --datadir=/var/lib/mysql
 	service mysql start
 	echo "CREATE DATABASE IF NOT EXISTS wordpress DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci" | mysql -u root
 	echo "CREATE USER IF NOT EXISTS '$MYSQL_ROOT_USER'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'" | mysql -u root
